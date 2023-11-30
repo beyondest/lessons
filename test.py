1e79e7c05db0ec46e7f1ddd642469897091fed36
ggbond <liyuxuan12345678@outlook.com>
2023-11-30 09:40:01 -0500

    a

--- test.py
+++ test.py
@@ -1,24 +1,15 @@
-#coding=utf-8
-import numpy as np
-import math
-import cv2
-path="rtsp://127.0.0.1:8080/h264_pcm.sdp"
-vd=cv2.VideoCapture(path)
-while True:
-    ret, frame = vd.read()  
-    if not ret:
-        break
-    cv2.imshow('ww', frame)  
+def counter():
+    a=[1,2,3,4,5]
+    while 1:
+        for i in range(5):
+            yield a[i]
+            a[i]+=1
+counter2=(i**2 for i in range(10))          
+a=counter()
+print(a)
+for i in range(10):
+    print(next(a))
+    
 
-    if cv2.waitKey(1) & 0xFF ==27:
-        break
-
-
-
-vd.release()  
-cv2.destroyAllWindows()  
-   
-
-        
         
         
